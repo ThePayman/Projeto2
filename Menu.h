b792ab3bc58a132539a8c0d9267dcab612b33502
Afoxys <38985979+Afoxys@users.noreply.github.com>
2018-05-10 18:50:34 +0100

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "iostream"
+#include "fstream"
+#include "string"
+#include "vector"
+
+using namespace std;
+
+
+class Menu
+{
+public:
+	Menu();
+
+	void Selection();
+
+
+	~Menu();
+
+private:
+	char option;
+	
+};
+
